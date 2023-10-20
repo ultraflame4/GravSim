3599814e1c925a0260e1544b98619ce9a5172e22
ultr42 <ultr42@skiff.com>
2023-10-20 15:44:25 +0800

    Copy assets on build CMakeLists.txt

--- assets/gravbody.fragment.glsl
+++ assets/gravbody.fragment.glsl
@@ -1,9 +1,9 @@
 
 #version 330 core
 out vec4 FragColor;
-in vec3 ourColor;
+in vec3 bgColor;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+    FragColor = vec4(bgColor, 1.0);
 }
--- assets/gravbody.vertex.glsl
+++ assets/gravbody.vertex.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 layout (location = 0) in vec2 aPos;
 
-out vec3 backgroundColor;
+out vec3 bgColor;
 
 void main()
 {
     gl_Position = vec4(aPos, 0, 1.0);
-    backgroundColor = vec3(0,0,0);
+    bgColor = vec3(0.1,0.1,0.1);
 }
