7d4f44587c3a4151e60d9853da4f4cfaca2483a7
ultr42 <ultr42@skiff.com>
2023-10-26 22:27:07 +0800

    bump gl version to 4.1

--- src/window.cc
+++ src/window.cc
@@ -8,8 +8,8 @@
 
 void Window::Init() {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfw_initialised = true;
