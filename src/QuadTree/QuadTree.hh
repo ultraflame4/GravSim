04e8933f055bba828f776ae83e1629426ca991e0
ultr42 <ultr42@skiff.com>
2023-12-05 20:35:23 +0800

    added quad tree stuff

---
+++ src/QuadTree/QuadTree.hh
@@ -0,0 +1,34 @@
+//
+// Created by powew on 5/12/2023.
+//
+
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <glm/glm.hpp>
+
+namespace QuadTree {
+
+    template<typename T>
+    class Node {
+    public:
+        std::unique_ptr<Node> children[4];
+        int depth;
+        std::vector<T> items;
+    };
+
+    template<typename T>
+    class QuadTreeManager {
+    public:
+        Node <T> rootNode;
+        float physicalSize;
+        glm::vec2 center;
+
+        explicit QuadTreeManager(float physicalSize  = 100.f, glm::vec2 center= glm::vec2(0,0));
+
+
+        const Node <T> &GetNodeFromPosition(glm::vec2 position);
+    };
+
+}
