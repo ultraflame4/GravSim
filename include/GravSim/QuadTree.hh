120c326d1bcecb242f27506d9aab581b97c7b86e
ultr42 <ultr42@skiff.com>
2023-12-05 21:08:24 +0800

    removed quad tree sources

--- include/GravSim/QuadTree.hh
+++ include/GravSim/QuadTree.hh
@@ -7,6 +7,8 @@
 #include <memory>
 #include <vector>
 #include <glm/glm.hpp>
+#include "GravSim/QuadTree.hh"
+
 
 namespace QuadTree {
 
@@ -29,4 +31,49 @@ namespace QuadTree {
         const Node <T> &GetNodeFromPosition(glm::vec2 position);
     };
 
+    template<typename T>
+    QuadTree::QuadTreeManager<T>::QuadTreeManager(float physicalSize, glm::vec2 center):physicalSize(physicalSize),
+                                                                                        center(center) {
+    }
+
+    template<typename T>
+    const QuadTree::Node<T> &QuadTree::QuadTreeManager<T>::GetNodeFromPosition(glm::vec2 position) {
+        glm::vec2 quad_center = QuadTree::QuadTreeManager<T>::center;
+        float halfSize = QuadTree::QuadTreeManager<T>::physicalSize / 2;
+        // pointer to the current quad
+        QuadTree::Node<T> *parent = &QuadTree::QuadTreeManager<T>::rootNode;
+        // pointer used when indexing children of search quad
+        QuadTree::Node<T> *current = &QuadTree::QuadTreeManager<T>::rootNode;
+
+        while (current != nullptr) {
+            parent = current;
+            if (position.y > quad_center.y) {
+                // Top left
+                if (position.x < quad_center.x) {
+                    current = parent->children[0].get();
+                    quad_center.x -= halfSize;
+                }
+                    // top right
+                else {
+                    current = parent->children[1].get();
+                    quad_center.x += halfSize;
+                }
+                quad_center.y += halfSize;
+            } else {
+                // bottom left
+                if (position.x < quad_center.x) {
+                    current = parent->children[2].get();
+                    quad_center.x -= halfSize;
+                }
+                    // bottom right
+                else {
+                    current = parent->children[3].get();
+                    quad_center.x += halfSize;
+                }
+                quad_center.y -= halfSize;
+            }
+            halfSize = halfSize / 2;
+        }
+        return parent;
+    }
 }
--- src/QuadTreeImpl.cc
+++
@@ -1,8 +0,0 @@
-//
-// Created by powew on 5/12/2023.
-//
-#include "GravSim/QuadTree.hh"
-#include "GravSim/GravitySimulation.hh"
-#include "QuadTree.cc"
-
-QuadTree::QuadTreeManager<GravBodyPhysical> temp;
