6978518f8ca6acc5b8d8feb03f8bf4ecf02167d3
ultr42 <ultraflame4@gmail.com>
2025-09-07 14:45:38 +0800

    Spin instead of thread sleep for smoother tps

--- src/window.cc
+++ src/window.cc
@@ -16,9 +16,9 @@ void Window::Init() {
 }
 
 Window::Window(int width, int height, const std::string &title) {
-    if (!glfw_initialised) Init();
+    if (!glfw_initialised)
+        Init();
     logger = logging::get<Window>(title);
-
 
     window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
     if (window == nullptr) {
@@ -27,9 +27,8 @@ Window::Window(int width, int height, const std::string &title) {
         throw std::runtime_error("Failed to create glfw window!");
     }
 
-
     glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         logger->critical("Failed to initialize GLAD");
         throw std::runtime_error("Failed to initialize GLAD!");
     }
@@ -42,7 +41,7 @@ Window::Window(int width, int height, const std::string &title) {
     glEnable(GL_BLEND);
     CheckGLErrors();
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-//    glfwSwapInterval(0);
+    //    glfwSwapInterval(0);
     CheckGLErrors();
     this->width = width;
     this->height = height;
@@ -56,24 +55,28 @@ Window::Window(int width, int height, const std::string &title) {
     CheckGLErrors();
 }
 
-void Window::static_framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+void Window::static_framebuffer_size_callback(GLFWwindow *window, int width,
+                                              int height) {
     windows_list[window]->framebuffer_size_callback(window, width, height);
 }
 
-void Window::static_key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+void Window::static_key_callback(GLFWwindow *window, int key, int scancode,
+                                 int action, int mods) {
     windows_list[window]->OnInput(key, action, mods);
 }
 
-void Window::static_mousebtn_callback(GLFWwindow *window, int key, int action, int mods) {
+void Window::static_mousebtn_callback(GLFWwindow *window, int key, int action,
+                                      int mods) {
     windows_list[window]->OnInput(key, action, mods);
 }
 
-
-void Window::static_scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+void Window::static_scroll_callback(GLFWwindow *window, double xoffset,
+                                    double yoffset) {
     windows_list[window]->OnScroll(xoffset, yoffset);
 }
 
-void Window::framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+void Window::framebuffer_size_callback(GLFWwindow *window, int width,
+                                       int height) {
     this->width = width;
     this->height = height;
     glViewport(0, 0, width, height);
@@ -82,21 +85,17 @@ void Window::framebuffer_size_callback(GLFWwindow *window, int width, int height
 
 void Window::_update_thread() {
     const int base_tps = this->updateTps;
-    const float delay = 1000.0f / base_tps;
-    const int delay_i = std::floor(delay);
-    float missed_time = 0;
+    const float delay = 1.0f / base_tps;
+
+    float next_update = 0;
     while (is_running) {
-        missed_time += updateTimer.delta * 1000 / delay;
 
-        int extra = std::floor(missed_time);
-        missed_time -= extra;
-
-        int total_cycles = extra + 1;
-        for (int i = 0; i < total_cycles; ++i) {
-            Update(updateTimer.tick());
+        float now = glfwGetTime();
+        if (next_update > now) {
+            continue;
         }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(delay_i));
+        Update(updateTimer.tick());
+        next_update = now + delay;
     }
 }
 
@@ -107,7 +106,7 @@ void Window::run() {
     updateTimer.tick();
     is_running = true;
     pUpdateThread = new std::thread(&Window::_update_thread, this);
-    
+
     Start();
 
     while (!glfwWindowShouldClose(window)) {
@@ -139,18 +138,21 @@ void Window::run() {
 }
 
 void Window::InitIMGUI() {
-// Setup Dear ImGui context
+    // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |=
+        ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |=
+        ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
     // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(window,
-                                 true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplGlfw_InitForOpenGL(
+        window,
+        true); // Second param install_callback=true will install GLFW callbacks
+               // and chain to existing ones.
     ImGui_ImplOpenGL3_Init();
 }
-
 
 std::shared_ptr<spdlog::logger> logger = logging::get("OpenGl");
 
@@ -159,29 +161,30 @@ GLenum CheckGLErrors_(const char *file, int line) {
     while ((errorCode = glGetError()) != GL_NO_ERROR) {
         std::string error;
         switch (errorCode) {
-            case GL_INVALID_ENUM:
-                error = "INVALID_ENUM";
-                break;
-            case GL_INVALID_VALUE:
-                error = "INVALID_VALUE";
-                break;
-            case GL_INVALID_OPERATION:
-                error = "INVALID_OPERATION";
-                break;
-            case GL_STACK_OVERFLOW:
-                error = "STACK_OVERFLOW";
-                break;
-            case GL_STACK_UNDERFLOW:
-                error = "STACK_UNDERFLOW";
-                break;
-            case GL_OUT_OF_MEMORY:
-                error = "OUT_OF_MEMORY";
-                break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION:
-                error = "INVALID_FRAMEBUFFER_OPERATION";
-                break;
+        case GL_INVALID_ENUM:
+            error = "INVALID_ENUM";
+            break;
+        case GL_INVALID_VALUE:
+            error = "INVALID_VALUE";
+            break;
+        case GL_INVALID_OPERATION:
+            error = "INVALID_OPERATION";
+            break;
+        case GL_STACK_OVERFLOW:
+            error = "STACK_OVERFLOW";
+            break;
+        case GL_STACK_UNDERFLOW:
+            error = "STACK_UNDERFLOW";
+            break;
+        case GL_OUT_OF_MEMORY:
+            error = "OUT_OF_MEMORY";
+            break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            error = "INVALID_FRAMEBUFFER_OPERATION";
+            break;
         }
-        logger->error("Error code {} : {} at \"{}\" line {}", errorCode, error, file, line);
+        logger->error("Error code {} : {} at \"{}\" line {}", errorCode, error,
+                      file, line);
     }
     return errorCode;
 }
