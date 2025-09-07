8fe5139bfe69b511c2bdb5e28d391a1b370639fa
ultr42 <ultraflame4@gmail.com>
2025-09-07 14:59:47 +0800

    added simulation type

---
+++ include/GravSim/simulation.hh
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <vector>
+
+struct SimulatedPhysicsBody {
+    glm::vec2 pos;
+    glm::vec2 vel;
+    float mass;
+    float radius;
+
+    void accelerate(glm::vec2 direction, float forceAmt) {
+        vel += direction * (forceAmt / mass);
+    }
+
+    void addImmediateForce(glm::vec2 direction, float forceAmt) {
+        vel = direction * (forceAmt / mass);
+    }
+};
+
+class Simulation {
+  public:
+    Simulation(float stepSize) : stepSize(stepSize) {}
+
+    float stepSize;
+
+  private:
+    std::vector<SimulatedPhysicsBody> bodies;
+
+  public:
+    int spawnBody(float x, float y, float radius, float mass) {
+        auto& bodyp = this->bodies.emplace_back(
+            SimulatedPhysicsBody{glm::vec2(x, y), glm::vec2(x, y), mass, radius}
+        );
+
+        return bodies.size() - 1;
+    }
+};
